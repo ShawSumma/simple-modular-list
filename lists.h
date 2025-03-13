aeb889b08086676bb1e39ae721e2d6115a26f2cf
Shaw Summa <shawsumma@gmail.com>
2025-03-13 03:24:23 -0400

    fixes
    Signed-off-by: Shaw Summa <shawsumma@gmail.com>

--- lists.h
+++ lists.h
@@ -9,6 +9,15 @@
 #define LIST(x) list_int_ ## x 
 #include "vector.inc"
 
-#define ELEM list_int_t
-#define LIST(x) list_list_int_ ## x
+#define ELEM void *
+#define LIST(x) list_obj_ ## x
 #include "vector.inc"
+
+#define list(T) list_ ## T ## _t
+#define list_new(T) list_ ## T ## _new()
+#define list_delete(T, v) list_ ## T ## _delete(v)
+#define list_push(T, v, o) list_ ## T ## _push(&v, o)
+#define list_index(T, v, i) (*list_ ## T ## _index(&v, (size_t) (i)))
+#define list_pop(T, v) list_ ## T ## _pop(&v)
+#define list_len(T, v) list_ ## T ## _size(&v)
+#define list_is_empty(T, v) list_ ## T ## _is_empty(&v)
--- test.c
+++ test.c
@@ -1,4 +1,20 @@
 
 #include "lists.h"
 
+#include <stdlib.h>
+
 int main() {
+    list(int) x = list_new(int);
+
+    for (int i = 0; i < 7; i++) {
+        list_push(int, x, i);
+    }
+
+    list_index(int, x, 3) = 30;
+
+    while (list_len(int, x) != 0) {
+        printf("%i\n", list_pop(int, x));
+    }
+
+    list_delete(int, x);
+}
